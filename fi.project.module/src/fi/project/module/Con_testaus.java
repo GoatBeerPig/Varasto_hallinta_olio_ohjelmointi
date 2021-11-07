82c5d71837590489aa53da7cf7fe29aacf20ea5b
GoatBeerPig <hamalainentomi37@gmail.com>
2021-11-07 16:09:52 +0200

    update

---
+++ fi.project.module/src/fi/project/module/Con_testaus.java
@@ -0,0 +1,35 @@
+package fi.project.module;
+
+
+
+import java.sql.*;
+
+public class Con_testaus {
+   public static void main(String[] args ) throws Exception {
+      // register Oracle thin driver with DriverManager service
+      // it is optional for JDBC4.x version
+      // Class.forName("com.mysql.cj.jdbc.Driver");
+    
+      // declare variables
+      // place your own values
+      final String url = "jdbc:mysql://192.168.8.144:3306/testivarasto";
+      final String user = "remuser";
+      final String password = "passu123";
+
+      // establish the connection
+      Connection con = DriverManager.getConnection(url, user, password);
+
+      // display status message
+      if(con == null) {
+         System.out.println("JDBC connection is not established");
+         return;
+      }
+      else 
+         System.out.println("Congratulations,"+
+            " JDBC connection is established successfully.\n");
+      // close JDBC connection
+      con.close();
+   } //main
+} //class
+
+
---
+++ fi.project.module/src/fi/project/module/Varasto.java
@@ -0,0 +1,71 @@
+package fi.project.module;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Varasto {
+   public static void main(String[] args) throws Exception {
+      // variables
+             final String url = "jdbc:mysql://192.168.8.144:3306/testivarasto";
+             final String user = "remuser";
+             final String password = "passu123"; 
+             
+             String sql_drop = "DROP TABLE toinen_varasto";
+
+             
+
+      // establish the connection
+      Connection con = DriverManager.getConnection(url, user, password);
+
+      // create JDBC statement object
+      Statement st = con.createStatement();
+      
+      
+      st.executeUpdate(sql_drop);
+      st.execute("create table toinen_varasto (id integer, name varchar(20))");                        ///tämmönen varmaan tarvii jonku iffin, joka checkaa onko table jo olemassa
+      st.execute("insert into toinen_varasto values(1,'tuote2')");
+
+
+      // prepare SQL query
+      String query = "SELECT id, NAME FROM varasto";
+
+      // send and execute SQL query in Database software
+      ResultSet rs2 = st.executeQuery("select * from toinen_varasto");
+      
+      if (rs2 != null)  
+          while (rs2.next()) 
+      {  
+          System.out.println("Tuotteen Id on: " + rs2.getString(1));  
+          System.out.println("Tuotteen nimi on: " + rs2.getString(2));  
+      }  
+      rs2.close();
+
+      // process the ResultSet object
+      boolean flag = false;
+      
+      ResultSet rs = st.executeQuery(query);
+
+      while (rs.next()) {
+         flag = true;
+         System.out.println("'" + rs.getInt(1) +
+                           "', '" + rs.getString(2) + "'");
+      }
+
+      if (flag == true) {
+         System.out.println("\nRecords retrieved and displayed");
+      } else {
+         System.out.println("Record not found");
+      }
+      
+      
+      
+      
+
+      // close JDBC objects
+      rs.close();
+      st.close();
+      con.close();
+   }
+}
